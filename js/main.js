94bd2fbff9cfd707880bf508936b8bccb8017486
didicozaur <moisin.diana@yahoo.com>
2022-04-11 19:16:37 +0200

    skeleton of the game

---
+++ js/main.js
@@ -0,0 +1,104 @@
+const cardsArr = [
+  {
+    name: "elephant",
+    img: "images/elephant.jpg",
+  },
+  {
+    name: "fox",
+    img: "images/fox.jpg",
+  },
+  {
+    name: "giraffe",
+    img: "images/giraffe.jpg",
+  },
+  {
+    name: "horse",
+    img: " images/horse.jpg",
+  },
+  {
+    name: "hummingbird",
+    img: "images/hummingbird.jpg",
+  },
+  {
+    name: "owl",
+    img: "images/owl.jpg",
+  },
+  {
+    name: "parrot",
+    img: "images/parrot.jpg",
+  },
+  {
+    name: "squirrel",
+    img: "images/squirrel",
+  },
+  {
+    name: "two birds",
+    img: "images/two birds.jpg",
+  },
+  {
+    name: "wood-pecker",
+    img: "images/wood-pecker.jpg",
+  },
+  {
+    name: "elephant",
+    img: "images/elephant.jpg",
+  },
+  {
+    name: "fox",
+    img: "images/fox.jpg",
+  },
+  {
+    name: "giraffe",
+    img: "images/giraffe.jpg",
+  },
+  {
+    name: "horse",
+    img: " images/horse.jpg",
+  },
+  {
+    name: "hummingbird",
+    img: "images/hummingbird.jpg",
+  },
+  {
+    name: "owl",
+    img: "images/owl.jpg",
+  },
+  {
+    name: "parrot",
+    img: "images/parrot.jpg",
+  },
+  {
+    name: "squirrel",
+    img: "images/squirrel",
+  },
+  {
+    name: "two birds",
+    img: "images/two birds.jpg",
+  },
+  {
+    name: "wood-pecker",
+    img: "images/wood-pecker.jpg",
+  },
+];
+
+cardsArr.sort(() => 0.5 - Math.random());
+
+const grid = document.querySelector("#grid");
+
+console.log("grid");
+
+function createBoard() {
+  for (let i = 0; i < cardsArr.length; i++) {
+    const card = document.createElement("img");
+    card.setAttribute("src", "images/question1.jpg");
+    card.setAttribute("data-id", "i");
+    card.addEventListener("click", turnCard);
+    grid.appendChild(card);
+    console.log(card, i);
+  }
+}
+createBoard();
+
+function turnCard() {
+  console.log("clicked");
+}
