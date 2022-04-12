24c8bc6332e53c8f2750f2a542417e3017a19160
didicozaur <moisin.diana@yahoo.com>
2022-04-13 00:17:03 +0200

    added functionality to the game

---
+++ js/cards.js
@@ -0,0 +1,66 @@
+const cardsArr = [
+  {
+    name: "elephant",
+    img: "images/cards/elephant.jpg",
+  },
+  {
+    name: "giraffe",
+    img: "images/cards/giraffe.jpg",
+  },
+  {
+    name: "horse",
+    img: " images/cards/horse.jpg",
+  },
+  {
+    name: "hummingbird",
+    img: "images/cards/hummingbird.jpg",
+  },
+  {
+    name: "parrot",
+    img: "images/cards/parrot.jpg",
+  },
+  {
+    name: "squirrel",
+    img: "images/cards/squirrel.jpg",
+  },
+  {
+    name: "two birds",
+    img: "images/cards/two birds.jpg",
+  },
+  {
+    name: "wood-pecker",
+    img: "images/cards/wood-pecker.jpg",
+  },
+  {
+    name: "elephant",
+    img: "images/cards/elephant.jpg",
+  },
+  {
+    name: "giraffe",
+    img: "images/cards/giraffe.jpg",
+  },
+  {
+    name: "horse",
+    img: " images/cards/horse.jpg",
+  },
+  {
+    name: "hummingbird",
+    img: "images/cards/hummingbird.jpg",
+  },
+  {
+    name: "parrot",
+    img: "images/cards/parrot.jpg",
+  },
+  {
+    name: "squirrel",
+    img: "images/cards/squirrel.jpg",
+  },
+  {
+    name: "two birds",
+    img: "images/cards/two birds.jpg",
+  },
+  {
+    name: "wood-pecker",
+    img: "images/cards/wood-pecker.jpg",
+  },
+];
--- js/main.js
+++ js/main.js
@@ -1,76 +1,56 @@
-const cardsArr = [
-  {
-    name: "elephant",
-    img: "images/cards/elephant.jpg",
-  },
-  {
-    name: "giraffe",
-    img: "images/cards/giraffe.jpg",
-  },
-  {
-    name: "horse",
-    img: " images/cards/horse.jpg",
-  },
-  {
-    name: "hummingbird",
-    img: "images/cards/hummingbird.jpg",
-  },
-  {
-    name: "parrot",
-    img: "images/cards/parrot.jpg",
-  },
-  {
-    name: "squirrel",
-    img: "images/cards/squirrel",
-  },
-  {
-    name: "two birds",
-    img: "images/cards/two birds.jpg",
-  },
-  {
-    name: "wood-pecker",
-    img: "images/cards/wood-pecker.jpg",
-  },
-  {
-    name: "elephant",
-    img: "images/cards/elephant.jpg",
-  },
-  {
-    name: "giraffe",
-    img: "images/cards/giraffe.jpg",
-  },
-  {
-    name: "horse",
-    img: " images/cards/horse.jpg",
-  },
-  {
-    name: "hummingbird",
-    img: "images/cards/hummingbird.jpg",
-  },
-  {
-    name: "parrot",
-    img: "images/cards/parrot.jpg",
-  },
-  {
-    name: "squirrel",
-    img: "images/cards/squirrel",
-  },
-  {
-    name: "two birds",
-    img: "images/cards/two birds.jpg",
-  },
-  {
-    name: "wood-pecker",
-    img: "images/cards/wood-pecker.jpg",
-  },
-];
-
 cardsArr.sort(() => 0.5 - Math.random());
 
 const grid = document.querySelector("#grid");
-const cardsChosen = [];
 
-console.log("grid");
+let cardsChosen = [];
+let cardsChosenIds = [];
+const cardsWon = [];
+let score = document.querySelector("#score");
+let scoreCount = 0;
+
+function checkForMatch() {
+  const cards = document.querySelectorAll("img");
+
+  let clickedFirstCard = cardsChosen[0];
+  let clickedSecondCard = cardsChosen[1];
+
+  let clickedFirstId = cardsChosenIds[0];
+  let clickedSecondId = cardsChosenIds[1];
+
+  if (clickedFirstCard === clickedSecondCard) {
+    cards[clickedFirstId].removeEventListener("click", turnCard);
+    cards[clickedSecondId].removeEventListener("click", turnCard);
+    console.log("You found a match!");
+    cardsChosenIds = [];
+    cardsChosen = [];
+    scoreCount++;
+    score.textContent = scoreCount;
+    cardsWon.push(clickedFirstCard);
+    console.log(cardsWon.length, cardsArr.length / 2);
+  } else if (cardsWon.length == cardsArr.length / 2) {
+    score.innerHTML = "You won!!!";
+  } else {
+    console.log("Sorry, try again!");
+    setTimeout(function () {
+      cards[clickedFirstId].setAttribute("src", "images/question1.jpg");
+      cards[clickedSecondId].setAttribute("src", "images/question1.jpg");
+
+      cardsChosenIds = [];
+      cardsChosen = [];
+    }, 500);
+  }
+}
+
+function turnCard() {
+  const cardId = this.getAttribute("data-id");
+
+  cardsChosen.push(cardsArr[cardId].name);
+  cardsChosenIds.push(cardId);
+  this.setAttribute("src", cardsArr[cardId].img);
+  if (cardsChosen.length === 2) {
+    checkForMatch();
+  }
+}
 
 function createBoard() {
   for (let i = 0; i < cardsArr.length; i++) {
@@ -79,16 +59,6 @@ function createBoard() {
     card.setAttribute("data-id", i);
     card.addEventListener("click", turnCard);
     grid.appendChild(card);
-    console.log(card, i);
   }
 }
-
 createBoard();
-
-function turnCard() {
-  const cardId = this.getAttribute("data-id");
-
-  cardsChosen.push(cardsArr[cardId].name);
-  console.log("clicked", cardId);
-  console.log(cardsChosen);
-}
