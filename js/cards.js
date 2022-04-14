e03d5db42c781104775746e1ea2dce82e19d7842
didicozaur <moisin.diana@yahoo.com>
2022-04-15 00:00:23 +0200

    added more cards to the game

--- js/cards.js
+++ js/cards.js
@@ -1,7 +1,39 @@
 const cardsArr = [
   {
+    name: "white cat",
+    img: "images/cards/white-cat.jpg",
+  },
+  {
+    name: "pangda",
+    img: "images/cards/pandas.jpg",
+  },
+  {
+    name: "leopard",
+    img: "images/cards/leopard.jpg",
+  },
+  {
+    name: "raton",
+    img: "images/cards/raton.jpg",
+  },
+  {
     name: "elephant",
     img: "images/cards/elephant.jpg",
+  },
+  {
+    name: "leopard",
+    img: "images/cards/leopard.jpg",
+  },
+  {
+    name: "raton",
+    img: "images/cards/raton.jpg",
+  },
+  {
+    name: "pangda",
+    img: "images/cards/pandas.jpg",
+  },
+  {
+    name: "white cat",
+    img: "images/cards/white-cat.jpg",
   },
   {
     name: "giraffe",
